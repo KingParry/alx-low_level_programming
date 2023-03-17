f65bd6c618383731bf134830ad557d75bdce126c
KingParry <kingparry@tahoo.com>
2023-03-17 07:50:20 -0700

    variables if_else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,15 +5,12 @@
  * main - Prints if the number is zero, positive or negative
  * Return : Always Success)
  *
- */
- 
+ */ 
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
@@ -22,7 +19,7 @@ int main(void)
 	{
 	printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
 	printf("%d is negative\n", n);
 	}
