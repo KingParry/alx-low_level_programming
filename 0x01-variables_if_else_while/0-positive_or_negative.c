68ab27a54d69d893928fcc59c9431ccfc57d48b9
KingParry <kingparry@tahoo.com>
2023-03-17 13:11:30 -0700

    variables,if_else,while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 /**
- * main - prints if number is positive, zero or negative
+ * main - Prints if number is positive, zero or negative
+ *
  * Return : Always (Success)
  *
  */
-
 int main(void)
 {
-        int n;
+	int n;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
