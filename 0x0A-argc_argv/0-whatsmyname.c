cc0774ce5e1c264ae17296b944467ca6f3ff73a2
KingParry <kingparry@tahoo.com>
2023-04-10 15:38:18 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main program start up
+ *
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
