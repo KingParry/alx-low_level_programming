d5882424c940b0f32357ea0fcb5c14e2bd6255b8
KingParry <kingparry@tahoo.com>
2023-04-10 18:43:05 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - start main program
+ * @argc: argument no.
+ * @argv: argument array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int r;
+
+	if (argc == 3)
+	{
+		r = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", r);
+		return (0);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+}
