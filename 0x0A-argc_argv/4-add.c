83a61842efadcb78dad4027c3a9c2047ef76172a
KingParry <kingparry@tahoo.com>
2023-04-10 19:51:57 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * main - main program
- * @argc: argument number
- * @argv: argument  arrays
- * Return: return (0)
+ * main -main function
+ * @argc: the count argument
+ * @argv: array of argument
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
 	int r, s, sum;
-	
+
 	for (r = 1; r < argc; r++)
 	{
 		for (s = 0; argv[r][s]; s++)
 		{
 			if (argv[r][s] < '0' || argv[r][s] > '9')
 			{
-				printf("%s\n", "Error");
+				printf("Error\n");
 				return (1);
 			}
 		}
